d2ac618844c7754ba9d0a0496a82521210810c5f
bet-io <98025375+bet-io@users.noreply.github.com>
2022-12-10 20:38:00 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -7,8 +7,9 @@ blog({
   avatar: "https://deno-avatar.deno.dev/avatar/83a531.svg",
   avatarClass: "rounded-full",
   links: [
-    { title: "Email", url: "mailto:dengyvjun0507@outlook.com },
+    { title: "Email", url: "mailto:bjimsblog@outlook.com" },
     { title: "GitHub", url: "https://github.com/bet-io" },
+    { title: "Twitter", url: "https://twitter.com/" },
   ],
   lang: "zh",
 });
