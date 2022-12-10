6d540524f299d323f986c3125daa921677c0ac77
bet-io <98025375+bet-io@users.noreply.github.com>
2022-12-10 21:58:12 +0800

    f

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import blog from "https://deno.land/x/blog/blog.tsx";
 
 blog({
-  author: "Dino",
+  author: "Jim",
   title: "Jim's Blog",
   description: "The blog description.",
   avatar: "https://deno-avatar.deno.dev/avatar/83a531.svg",
@@ -10,6 +10,7 @@ blog({
     { title: "Email", url: "mailto:bjimsblog@outlook.com" },
     { title: "GitHub", url: "https://github.com/bet-io" },
     { title: "Twitter", url: "https://twitter.com/" },
+    { title: "Bilibili", url: "https://space.bilibili.com/699324580" }
   ],
   lang: "zh",
 });
